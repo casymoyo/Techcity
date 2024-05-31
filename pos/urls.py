554697a0f780f38ee95efd87f439f25f7a877dc5
casper moyo <cassymyo@gmail.com>
2024-05-31 14:47:49 +0200

    'requirement'

--- pos/urls.py
+++ pos/urls.py
@@ -4,5 +4,5 @@ from . views import *
 app_name = 'pos'
 
 urlpatterns = [
-    path('', pos, name='pos')
+    path('pos/', pos, name='pos')
 ]
--- techcity/settings.py
+++ techcity/settings.py
@@ -115,12 +115,12 @@ DATABASES = {
     #     'PORT': os.environ.get('DB_PORT', '5432'),
     # }
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE'),
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'DB_ENGINE':'django.db.backends.postgresql',
+        'DB_NAME':'railway',
+        'DB_USER':'postgres',
+        'DB_PASSWORD':'xJbUBjfjGZrMjXOBGYqKDREUEuTUzciV',
+        'DB_HOST':'monorail.proxy.rlwy.net',
+        'DB_PORT':'39004'
     }
 
 }
