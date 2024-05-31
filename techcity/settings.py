7484584de3e7d9cc2031b096906754f8037ed1d9
casper moyo <cassymyo@gmail.com>
2024-05-31 14:11:59 +0200

    'requirements'

--- techcity/settings.py
+++ techcity/settings.py
@@ -3,9 +3,10 @@ Django settings for techcity project.
 """
 import environ, os
 from pathlib import Path
+from dotenv import load_dotenv
 from celery.schedules import crontab
 
-env = environ.Env()
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -113,13 +114,13 @@ DATABASES = {
     #     'HOST': os.environ.get('DB_HOST', 'localhost'),
     #     'PORT': os.environ.get('DB_PORT', '5432'),
     # }
-        'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'xJbUBjfjGZrMjXOBGYqKDREUEuTUzciV',
-        'HOST': 'monorail.proxy.rlwy.net',
-        'PORT': '39004',
+    'default': {
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 
 }
@@ -249,13 +250,24 @@ AWS_ACCESS_KEY_ID = 'AKIAQP45RRLGRXGVLDQU'
 AWS_SECRET_ACCESS_KEY = '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'  
 AWS_STORAGE_BUCKET_NAME = 'techcity'
 AWS_S3_REGION_NAME = 'us-east-1'  
-AWS_S3_FILE_OVERWRITE = False 
 
-# File Storage Configuration
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % 'techcity'
+
+TWILIO_WHATSAPP_NUMBER = os.getenv('TWILIO_WHATSAPP_NUMBER')
+TWILIO_ACCOUNT_SID = os.getenv('TWILIO_ACCOUNT_SID')
+TWILIO_AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN')
+
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')
+AWS_S3_FILE_OVERWRITE = False 
 
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % 'techcity'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
