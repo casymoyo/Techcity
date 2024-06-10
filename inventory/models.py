ef869b103c5568e698f18a55b7d94f38a7ea5c1b
casper moyo <cassymyo@gmail.com>
2024-06-10 10:52:38 +0200

    cashbook

--- inventory/models.py
+++ inventory/models.py
@@ -47,15 +47,8 @@ class Inventory(models.Model):
         return f'{self.branch.name} : ({self.product.name}) quantity ({self.quantity})'
     
 class Transfer(models.Model):
+    batch_code = models.CharField(max_length=20)
     transfer_ref = models.CharField(max_length=20)
-    from_branch = models.ForeignKey(Branch, on_delete=models.CASCADE, related_name='destination')
-    to_branch = models.ForeignKey(Branch, on_delete=models.CASCADE, related_name='source')
-    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
-    quantity = models.IntegerField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    date = models.DateField(auto_now_add=True)
-    received = models.BooleanField(default=False)
-    declined = models.BooleanField(default=False)
     
     @classmethod
     def generate_transfer_number(cls):
@@ -72,6 +65,20 @@ class Transfer(models.Model):
     
     def __str__(self):
         return self.product.name
+
+class TransferItems(models.Model):
+    transfer_ref = models.CharField(max_length=20)
+    from_branch = models.ForeignKey(Branch, on_delete=models.CASCADE, related_name='destination')
+    to_branch = models.ForeignKey(Branch, on_delete=models.CASCADE, related_name='source')
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField(auto_now_add=True)
+    received = models.BooleanField(default=False)
+    declined = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.product.name} to {self.to_branch}'
     
 
 class DefectiveProduct(models.Model):
