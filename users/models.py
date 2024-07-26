2a49500cdb8a2ac2403e1dea2f14f958231b99f9
Leon Dhimba <dhimbaleon356@gmail.com>
2024-07-26 10:26:17 +0200

    feat[users]:relate user to a company

--- users/models.py
+++ users/models.py
@@ -7,12 +7,12 @@ This module manages user models and related functionality, including:
 * **Code Generation:** Implements logic to generate random unique codes for each user. 
 """
 
-import random,  string
+import random, string
 from django.db import models
 from django.apps import apps
 from company.models import Branch
 from django.contrib.auth.models import AbstractUser, Group
-from django.db.models .signals import post_migrate, post_save
+from django.db.models.signals import post_migrate, post_save
 
 ADMIN_GROUP_NAME = 'Admin'
 ACCOUNTANT_GROUP_NAME = 'Accountant'
@@ -21,10 +21,12 @@ SALESPERSON_GROUP_NAME = 'Salesperson'
 from django.contrib.auth.base_user import BaseUserManager
 from django.utils.translation import gettext_lazy as _
 
+
 class CustomUserManager(BaseUserManager):
     """
     Custom user model manager with extra functionalities.
     """
+
     def create_user(self, email, password, **extra_fields):
         """
         Creates and saves a User with the given email, password and extra fields.
@@ -34,11 +36,11 @@ class CustomUserManager(BaseUserManager):
             raise ValueError('The Email field is required')
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
-        user.is_staff = True 
+        user.is_staff = True
         user.save(using=self._db)
-        
-        if self.model.objects.count() == 1:  
-            user.is_superuser = True  
+
+        if self.model.objects.count() == 1:
+            user.is_superuser = True
             user.groups.add(Group.objects.get_or_create(name='Admin')[0])
         user.save(using=self._db)
         return user
@@ -58,22 +60,22 @@ class CustomUserManager(BaseUserManager):
 
         return self.create_user(email, password, **extra_fields)
 
-    
+
 class User(AbstractUser):
-    
     USER_ROLES = (
         ('admin', 'Admin'),
         ('sales', 'Salesperson'),
-        ('accountant', 'Accountant')    
+        ('accountant', 'Accountant')
     )
-    
+
     profile_image = models.ImageField(upload_to='Profile_images', blank=True, null=True)
+    company = models.ForeignKey('company.Company', on_delete=models.CASCADE, null=True)
     branch = models.ForeignKey('company.Branch', on_delete=models.CASCADE, null=True)
-    code = models.CharField(max_length=50, null=True, blank=True) 
+    code = models.CharField(max_length=50, null=True, blank=True)
     groups = models.ManyToManyField(Group)
     phonenumber = models.CharField(max_length=13)
     role = models.CharField(choices=USER_ROLES, max_length=50)
-    
+
     def code_generator(self) -> str:
         length = 5
         chars = string.ascii_uppercase + string.digits
@@ -81,26 +83,33 @@ class User(AbstractUser):
             code = ''.join(random.choice(chars) for _ in range(length))
             if not User.objects.filter(code=code).exists():
                 return code
-    
+
     def save(self, *args, **kwargs):
-        if not self.code:  
+        if not self.code:
             self.code = self.code_generator()
-        super().save(*args, **kwargs) 
-    
+        super().save(*args, **kwargs)
+
+        # validate that the user's branch is associated with the same company
+        if self.branch and self.company and self.branch.company != self.company:
+            raise ValueError('The branch does not belong to the specified company')
+
     def __str__(self) -> str:
         return self.username
-    
+
     objects = CustomUserManager()
 
+
 def assign_admin_group(sender, instance, created, **kwargs):
-    if created and instance.is_superuser: 
+    if created and instance.is_superuser:
         admin_group = Group.objects.get(name=ADMIN_GROUP_NAME)
         instance.groups.add(admin_group)
+
 
 def create_groups(sender, **kwargs):
     Group.objects.get_or_create(name=ADMIN_GROUP_NAME)
     Group.objects.get_or_create(name=ACCOUNTANT_GROUP_NAME)
     Group.objects.get_or_create(name=SALESPERSON_GROUP_NAME)
-        
+
+
 post_save.connect(assign_admin_group, sender=User)
 post_migrate.connect(create_groups, sender=apps.get_app_config('users'))
