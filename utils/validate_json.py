26c5c16afc21b66f7c88657e812d378e9617065b
Leon Dhimba <dhimbaleon356@gmail.com>
2024-07-30 12:51:43 +0200

    feat:add password validation

--- utils/validate_json.py
+++ utils/validate_json.py
@@ -1,6 +1,6 @@
 def validate_company_registration_payload(payload):
     required_company_fields = ['name', 'description', 'address', 'domain', 'logo', 'email', 'phone_number']
-    required_user_fields = ['first_name', 'last_name', 'user_name', 'email', 'phonenumber', 'password']
+    required_user_fields = ['first_name', 'last_name', 'user_name', 'email', 'phonenumber', 'password1', 'password2']
 
     for field in required_company_fields:
         if field not in payload['company_data']:
@@ -9,5 +9,10 @@ def validate_company_registration_payload(payload):
     for field in required_user_fields:
         if field not in payload['user_data']:
             return False, f"Missing user field: {field}"
+
+        # validate password fields
+        if field == 'password1' or field == 'password2':
+            if payload['user_data']['password1'] != payload['user_data']['password2']:
+                return False, "Passwords do not match, please try again"
 
     return True, ""
