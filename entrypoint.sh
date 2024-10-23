c3f1443e177e4b885fa0aa65f85f5523312f902c
Casper Moyo <114152550+casymoyo@users.noreply.github.com>
2024-10-23 15:25:55 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,1 @@
-#!/bin/bash
 
-# Wait for PostgreSQL to be available
-while ! nc -z db 5432; do
-  echo "Waiting for PostgreSQL..."
-  sleep 1
-done
-
-# Apply database migrations
-python manage.py migrate
-
-# Collect static files
-python manage.py collectstatic --noinput
-
-# Start Gunicorn to serve the Django app
-gunicorn techcity.wsgi:application --bind 0.0.0.0:8000 --workers 4
-
-# Start NGINX
-nginx -g 'daemon off;'
